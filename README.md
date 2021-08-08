6b8aa4fdcaaec4d00eccf6044e57ba3bd8ff50b1
Kirti Parihar <pariharkirti24@gmail.com>
2021-08-08 21:47:59 +0530

    Basic readme and comments added

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Toy Robot Simulator
+### Installation
+```
+$ git clone https://github.com/Kirtiparihar24/toy_simulator.git
+$ cd toy_simulator
+$ bundle install
+```
+
+### Run test cases
+```
+$ bundle exec rspec
+```
+
+### Run this from standard input:
+```
+$ ruby start_game.rb
+```
--- robot.rb
+++ robot.rb
@@ -7,6 +7,8 @@ class Robot
     @max_unit = max_unit
   end
 
+  # Save position of toy robot
+  # Return nil if not value not given in format PLACE X,Y,DIRECTION
   def position=(cmd)
     coordinates_arr = cmd.gsub(/place/, "").split(",").map(&:strip)
     return nil unless coordinates_arr.count == 3 && SEQUENCE.include?(coordinates_arr[2])
@@ -17,14 +19,19 @@ class Robot
   end
 
   def roam(cmd)
+    # Rotate toy towards left direction, ex. if toy facing east then will face north
     if cmd == 'left'
       @position[:direction] = SEQUENCE[SEQUENCE.index(@position[:direction]) - 1]
     end
 
+    # Rotate toy towards right direction, ex. if toy facing east then will face south
     if cmd == 'right'
       @position[:direction] = SEQUENCE[SEQUENCE.index(@position[:direction]) + 1]
     end
 
+    # Move toy 1 step ahead on x axis if direction is east or west
+    # Move toy 1 step ahead on y axis if direction is north or south
+    # Position remain unchanged if attempt to move toy beyond set dimensions ex. 5
     if cmd == 'move'
       if @position[:direction] == 'north' || @position[:direction] == 'south'
         @position[:y] = @position[:y] + 1 if (@position[:y] + 1 <= max_unit)
@@ -34,6 +41,8 @@ class Robot
     end
   end
 
+  # Return Final position of the toy in string format ex. "0,2,west"
+  # Return "Invalid command" if position is nil due to invalid input
   def report
     @position.values.join(',') rescue "Invalid commands"
   end
